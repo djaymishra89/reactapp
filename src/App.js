47435df2efab619b402c6c38317766fa0113897f
Djay <dhananjay.msihra@venture7.com>
2025-04-30 19:20:45 +0530

    First

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Hello from './components/Hello';
+
+function App() {
+  return (
+    <div style={{ fontFamily: 'sans-serif', padding: '2rem' }}>
+      <h1>Welcome to React Sample App</h1>
+      <Hello name="DevOps Candidate" />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Hello.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Hello({ name }) {
+  return <p>Hello, {name}! This is a sample React component.</p>;
+}
+
+export default Hello;
